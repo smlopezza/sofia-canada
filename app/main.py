bb3a58b5dbc0047cd176ad3c2ff39b56be16b734
Sandra Lopez-Zamora <sandra@Sandras-MacBook-Pro.local>
2026-05-23 18:22:18 -0400

    Add SofIA avatar and static folder

--- app/main.py
+++ app/main.py
@@ -1,12 +1,16 @@
 from dotenv import load_dotenv
 load_dotenv()
 
+from pathlib import Path
 from fastapi import FastAPI
+from fastapi.staticfiles import StaticFiles
 from app.webhook import router as webhook_router
 from app.jobs import router as jobs_router
 from app.export import router as export_router
 
 app = FastAPI()
+
+app.mount("/static", StaticFiles(directory=str(Path(__file__).parent / "static")), name="static")
 
 app.include_router(webhook_router)
 app.include_router(jobs_router)
